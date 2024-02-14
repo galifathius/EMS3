d60a2fdcc8668a7e4f4eb7d24cfeaf8bfba620dc
alihamza <galifathiusx@gmail.com>
2024-02-14 16:25:27 +0500

    Initial Commit

---
+++ EMAdmin/admin.py
@@ -0,0 +1,67 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+
+# Inlines
+
+class EmailInline(admin.StackedInline):
+    model = Email
+    extra = 0
+
+
+class ContactNumberInline(admin.StackedInline):
+    model = ContactNumber
+    extra = 0
+
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    extra = 0
+
+
+class EducationInline(admin.StackedInline):
+    model = Education
+    extra = 0
+
+
+class TeachingExperienceInline(admin.StackedInline):
+    model = TeachingExperience
+    extra = 0
+
+
+class Other_Professional_ExperienceInline(admin.StackedInline):
+    model = Other_Professional_Experience
+    extra = 0
+
+
+class SkillInline(admin.StackedInline):
+    model = Professional_Skills
+    extra = 0
+
+
+class Religious_Or_Political_AfInline(admin.StackedInline):
+    model = Religious_Or_Political_Af
+    extra = 0
+
+
+class ReferencesInline(admin.StackedInline):
+    model = References
+    extra = 0
+
+
+# Inline over
+@admin.register(PersonalInfo)
+class PersonalInfoAdmin(admin.ModelAdmin):
+    inlines = [EmailInline,
+               ContactNumberInline,
+               AddressInline,
+               EducationInline,
+               TeachingExperienceInline,
+               Other_Professional_ExperienceInline,
+               SkillInline,
+               Religious_Or_Political_AfInline,
+               ReferencesInline
+               ]
+    list_display = ('first_name', 'last_name', 'gender', 'cnic', 'date_of_birth')
---
+++ EMAdmin/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+
+
+# Create your models here.
+class PersonalInfo(models.Model):
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('non-binary', 'Non-binary'),
+    )
+
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    cnic = models.BigIntegerField(max_length=13)
+    cnicFront = models.ImageField(null=True)
+    cnicBack = models.ImageField(null=True)
+    image = models.ImageField(null=True)
+    date_of_birth = models.DateField()
+
+
+# Inline Models
+class Address(models.Model):
+    ADDRESS_TYPE_CHOICES = (
+        ('present', 'Present'),
+        ('permanent', 'Permanent'),
+    )
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+    address_type = models.CharField(max_length=50, choices=ADDRESS_TYPE_CHOICES)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+
+class ContactNumber(models.Model):
+    CONTACT_TYPE_CHOICES = (
+        ('current', 'Current'),
+        ('alternative', 'Alternative'),
+    )
+    contact_type = models.CharField(max_length=50, choices=CONTACT_TYPE_CHOICES)
+    contact = models.BigIntegerField(max_length=11)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class Email(models.Model):
+    EMAIL_TYPE_CHOICES = (
+        ('home', 'Home'),
+        ('office', 'Office'),
+    )
+    email_type = models.CharField(max_length=50, choices=EMAIL_TYPE_CHOICES)
+    email = models.CharField(max_length=50)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class Education(models.Model):
+    title_of_degree = models.CharField(max_length=50)
+    institute = models.CharField(max_length=50)
+    grade = models.CharField(max_length=2)
+    year_of_passing = models.IntegerField(max_length=4)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class TeachingExperience(models.Model):
+    subject_taught = models.CharField(max_length=50)
+    institution = models.CharField(max_length=50)
+    teaching_period_from = models.DateField()
+    teaching_period_to = models.DateField()
+    age_group_taught = models.IntegerField(max_length=2)
+    references_name = models.CharField(max_length=50)
+    references_contact_no = models.BigIntegerField(max_length=11)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class Other_Professional_Experience(models.Model):
+    title_of_job = models.CharField(max_length=50)
+    organization_name = models.CharField(max_length=50)
+    job_period_from = models.DateField()
+    job_period_to = models.DateField()
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class Professional_Skills(models.Model):
+    skill_description = models.CharField(max_length=50)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class Religious_Or_Political_Af(models.Model):
+    position = models.CharField(max_length=50)
+    name_of_party = models.CharField(max_length=50)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+
+
+class References(models.Model):
+    name = models.CharField(max_length=50)
+    relationship = models.CharField(max_length=50)
+    contact_no = models.BigIntegerField(max_length=11)
+    personal_info = models.ForeignKey(PersonalInfo, on_delete=models.CASCADE)
+# Inline models over
+
